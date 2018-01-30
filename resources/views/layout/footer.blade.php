66ca1bf22074ac0a942614bf197334634c631de8
sagautam5 <sagautam5@gmail.com>
2018-01-30 17:55:46 +0545

    addition of responsive social icons at footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,12 +3,14 @@
 	    <div class="row">
 	    	<div class="col-md-12 col-sm-12">
 	    		<ul class="social">
-	    			<li class="facebook" ><a href="{{URL::to('https://www.facebook.com/sagar.gautam.583')}}" target="_blank"><i class="fa fa-facebook social-icon" aria-hidden="true"></i></a></li>
-	    			<li class = "twitter" ><a href="{{URL::to('https://twitter.com/RP_no_16')}}" target="_blank"><i class="fa fa-twitter social-icon" aria-hidden="true"></i></a></li>
-	    			<li class = "instagram" ><a href="{{URL::to('https://www.instagram.com/sagautam5/')}}" target="_blank"><i class="fa fa-instagram social-icon" aria-hidden="true"></i></a></li>
-	    			<li class = "github" ><a href="{{URL::to('https://github.com/sagautam5')}}" target="_blank"><i class="fa fa-github social-icon" aria-hidden="true"></i></a></li>
-	    			<li class = "stack-overflow" ><a href="{{URL::to('https://stackoverflow.com/users/6708661/sagar-gautam')}}" target="_blank"><i class="fa fa-stack-overflow social-icon" aria-hidden="true"></i></a></li>
-	    			<li class = "linkedin" ><a href="{{URL::to('https://www.linkedin.com/in/sagar-gautam-b7a030132/')}}" target="_blank"><i class="fa fa-linkedin social-icon" aria-hidden="true"></i></a></li>
+	    			<div class="col-md-12">
+	    				<li class = "facebook" ><a href="{{URL::to('https://www.facebook.com/sagar.gautam.583')}}" target="_blank"><i class="fa fa-facebook social-icon" aria-hidden="true"></i></a></li>
+	    				<li class = "twitter" ><a href="{{URL::to('https://twitter.com/RP_no_16')}}" target="_blank"><i class="fa fa-twitter social-icon" aria-hidden="true"></i></a></li>
+	    				<li class = "instagram" ><a href="{{URL::to('https://www.instagram.com/sagautam5/')}}" target="_blank"><i class="fa fa-instagram social-icon" aria-hidden="true"></i></a></li>
+	    				<li class = "github" ><a href="{{URL::to('https://github.com/sagautam5')}}" target="_blank"><i class="fa fa-github social-icon" aria-hidden="true"></i></a></li>
+	    				<li class = "stack-overflow" ><a href="{{URL::to('https://stackoverflow.com/users/6708661/sagar-gautam')}}" target="_blank"><i class="fa fa-stack-overflow social-icon" aria-hidden="true"></i></a></li>
+	    				<li class = "linkedin" ><a href="{{URL::to('https://www.linkedin.com/in/sagar-gautam-b7a030132/')}}" target="_blank"><i class="fa fa-linkedin social-icon" aria-hidden="true"></i></a></li>
+					</div>	    				
 	    		</ul>
 	    	</div>
 		  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
