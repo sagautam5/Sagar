7c64b7a6eea0d1db23ee3d68f81a65a9c201bdb2
sagautam5 <sagautam5@gmail.com>
2018-03-10 11:25:58 +0545

    content added to services page

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -18,25 +18,25 @@
         <div class="col-md-12">
           <h3 class="section-title"> Services</h3>
           <div class="section-title-divider"></div>
-          <p class="service-quote">"We organize our software development process in accordance with your project requirement and time limits."</p>
+          <p class="service-quote">"We organize our software development process to meet your project requirements with in your time limit"</p>
         </div>
       </div>
 
       <div class="row">
         <div class="col-md-4 service-item">
           <div class="service-icon"><i class="fa fa-desktop"></i></div>
-          <h4 class="service-title"><a href="{{URL::to('web/development')}}">Web development</a></h4>
-          <p class="service-description">We design and build general websites such as static websites, corporate websites and different types of functional websites . . . . <a href="{{URL::to('web/development')}}">read more >> </a></p>
+          <h4 class="service-title"><a>Website Design</a></h4>
+          <p class="service-description">We design and build general websites such as static websites, corporate websites and different types of functional websites</p>
         </div>
         <div class="col-md-4 service-item">
           <div class="service-icon"><i class="fa fa-paper-plane"></i></div>
-          <h4 class="service-title"><a href="">Java Programming</a></h4>
-          <p class="service-description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla dolore eu fugiat nulla fugiat nulla pariatur . . . . <a href="{{URL::to('java/programming')}}">read more >> </a></p>
+          <h4 class="service-title"><a>Web App development</a></h4>
+          <p class="service-description">We build web application based on required functionalities, design responsive and friendly user interface.</p>
         </div>
         <div class="col-md-4 service-item">
           <div class="service-icon"><i class="fa fa-bar-chart"></i></div>
-          <h4 class="service-title"><a href="">Dorwhjber Bosfndj</a></h4>
-          <p class="service-description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata  tarad limino ata . . . . <a href="{{URL::to('java/programming')}}">read more >></a></p>
+          <h4 class="service-title"><a>Java Programming</a></h4>
+          <p class="service-description">We develop applications and implement functionalites using advance Java programming </a></p>
         </div>
       </div>
     </div>
